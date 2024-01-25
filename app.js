8c0fb2a945a3757ad79c78a588943371214bce2d
Fidel <fidelhernandez_8@hotmail.com>
2024-01-24 22:58:42 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+let gameScore = 5;
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    let choice = ["Rock", "Scissors", "Paper"];
+    let ComputerChoice = choice[Math.floor(Math.random() * 3)]
+    return ComputerChoice.toLowerCase()
+}
+
+function playerChoice() {
+    let playChoice = null;
+    while (playChoice === null) {
+        playChoice = prompt("Choose from these 3 choices: Rock, Paper, or Scissors?");
+    }
+    return playChoice.toLowerCase();
+}
+
+function playRound(player, computer) {
+    if (player === computer) {
+        console.log("It's a tie! Play again.");
+        playRound(getComputerChoice(), playerChoice)();
+    }
+
+    else if ((player === 'rock' && computer === 'scissors') || (player === 'paper' && computer === 'rock') || (player === 'scissors' && computer === 'paper')) {
+        playerScore++;
+    }
+
+    else {
+        computerScore++;
+    }
+}
+
+function game() {
+    for (let i = 0; i < gameScore; i++) {
+        playRound(getComputerChoice(), playerChoice());
+    }
+}
+
+game();
